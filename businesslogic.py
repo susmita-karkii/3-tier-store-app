4c4af1883e4962243db8e24cb5e9034d49e64619
susmita-karkii <85597124+susmita-karkii@users.noreply.github.com>
2023-06-25 19:03:23 +0545

    Update businesslogic.py

--- businesslogic.py
+++ businesslogic.py
@@ -1,82 +1,24 @@
-# business logic
-import dataaccess as da
+from dataaccess import DataAccess
 
-# connecting to the database
-my_db = da.DataBaseManagement('three_layered_db.db')
+class ProductLogic:
+    def __init__(self):
+        self.data_access = DataAccess()
 
-# Product class 
-# initialize a class with customer profile and name of the product
-class Product:
-    basket_id = 1
-    def __init__(self, first_name, last_name, product_name):
-        self.first_name, self.last_name, self.product_name = first_name, last_name, product_name
+    def insert_product(self, first_name, last_name, product_name):
+        self.data_access.insert_product(first_name, last_name, product_name)
 
-    def __str__(self):
-        return self.first_name + ' ' + self.last_name + ' ' + self.product_name
+    def get_purchases(self):
+        purchases = self.data_access.get_purchases()
+        purchase_list = []
+        for purchase in purchases:
+            purchase_list.append({
+                'firstName': purchase[0],
+                'lastName': purchase[1],
+                'productName': purchase[2],
+                'productPrice': purchase[3]
+            })
+        return purchase_list
 
-# inserting a query into the database
-    def insert(self):
-        check_customer_existance = True
-        try:
-            customer_id = my_db.show(f"""
-                    SELECT c.CustomerId FROM Customer as c WHERE CustomerFirstName="{self.first_name}"\
-                            AND CustomerLastName="{self.last_name}"
-                    """).pop()[0]
-        except IndexError:
-            check_customer_existance = False
-
-        product_id = my_db.show(f"""
-                                SELECT p.ProductId FROM Product as p WHERE ProductName="{self.product_name}"
-                        """).pop()[0]
-
-        price = my_db.show(f"""
-                        SELECT pp.ProductPrice FROM Product_Price as pp INNER JOIN Product as p ON pp.ProductId=p.ProductId 
-                        WHERE ProductName="{self.product_name}"
-                """).pop()[0]
-
-        if  not check_customer_existance:   
-            my_db.insert(f"""INSERT INTO Customer (CustomerFirstName, CustomerLastName) VALUES \
-                            ("{self.first_name}", "{self.last_name}")""")
-
-            customer_id = my_db.show(f"""
-                    SELECT c.CustomerId FROM Customer as c WHERE CustomerFirstName="{self.first_name}"\
-                            AND CustomerLastName="{self.last_name}"
-                    """).pop()[0]
-
-            my_db.insert(f"""INSERT INTO Basket (CustomerId, OrderDate, SumPrice) VALUES \
-                        ({customer_id}, date('now'), {price} \
-                            )""")
-
-            my_db.insert(f"""INSERT INTO Basket_Product (BasketId, ProductId) VALUES ({Product.basket_id}, {product_id})""")
-            Product.basket_id += 1
-
-        else:
-            my_db.insert(f"""INSERT INTO Basket (CustomerId, OrderDate, SumPrice) VALUES \
-            ({customer_id}, date('now'), {price} \
-                )""")
-
-            my_db.insert(f"""INSERT INTO Basket_Product (BasketId, ProductId) VALUES ({Product.basket_id}, {product_id})""")
-            Product.basket_id += 1
-
-
-# showing the queries inserted by the user
-    @staticmethod
-    def show():
-        q = my_db.show(f"""
-                SELECT c.CustomerFirstName, c.CustomerLastName, p.ProductName, pp.ProductPrice FROM Customer as c \
-                INNER JOIN Basket as b ON c.CustomerId=b.CustomerId INNER JOIN Basket_Product as bp ON b.BasketId=bp.BasketId 
-                INNER JOIN Product as p ON p.ProductId = bp.ProductId INNER JOIN Product_Price as pp ON 
-                pp.ProductId=p.ProductId;
-            """)
-                                
-        return q
-
-# showing what are the products a customer has bought
-    @staticmethod
-    def show_history(fullname):
-        q = my_db.show(f"""SELECT p.ProductName, pp.ProductPrice FROM Customer as c \
-            INNER JOIN Basket as b ON c.CustomerId=b.CustomerId INNER JOIN Basket_Product as bp ON b.BasketId=bp.BasketId 
-            INNER JOIN Product as p ON p.ProductId = bp.ProductId INNER JOIN Product_Price as pp ON 
-            pp.ProductId=p.ProductId WHERE c.CustomerFirstName="{fullname[0]}" AND c.CustomerLastName="{fullname[1]}"
-            """)
-        return q
+    def get_purchase_history(self, full_name):
+        history = self.data_access.get_purchase_history(full_name)
+        return history
